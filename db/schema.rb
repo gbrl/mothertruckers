100dad235ebbb198f43af97da9b5330d36dcbd94
winterreise <puiwing.tam@gmail.com>
2016-05-19 16:35:40 +0000

    Implement stop model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519145811) do
+ActiveRecord::Schema.define(version: 20160519162122) do
+
+  create_table "stops", force: :cascade do |t|
+    t.integer  "truck_id"
+    t.string   "name"
+    t.datetime "from"
+    t.datetime "to"
+    t.float    "latitude"
+    t.float    "longitude"
+  end
 
   create_table "trucks", force: :cascade do |t|
     t.string   "name"
