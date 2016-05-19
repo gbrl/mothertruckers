7e081cb2ff32db9ab0d95dce6b75d5e5a37ffc19
winterreise <puiwing.tam@gmail.com>
2016-05-19 17:48:55 +0000

    Create rating migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519162122) do
+ActiveRecord::Schema.define(version: 20160519173132) do
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "score"
+    t.integer  "truck_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "stops", force: :cascade do |t|
     t.integer  "truck_id"
