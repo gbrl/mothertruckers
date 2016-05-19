a568966988b5dd0b1433e3941b5c273753d43f9b
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-19 16:32:28 -0400

    Begin basic testing, need more migrations

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -2,9 +2,9 @@ class Truck < ActiveRecord::Base
   before_save :make_slug
 
   has_many :notes, dependent: :destroy
-  has_many :favourites, dependent: :destroy
   has_many :users, through: :favourites
   has_many :stops, dependent: :destroy
+  has_many :ratings
 
   private
 
