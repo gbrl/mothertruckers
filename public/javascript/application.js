191c3f9fa64b4492aa6446cb02900d8de08f514c
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-20 12:47:23 -0400

    Adding live search filtering WIP

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
-  $('#all-trucks').masonry({
+
+  // LAUNCH MASONRY
+  $grid = $('#all-trucks');
+  $grid.masonry({
   // options
-  itemSelector: '.truck-box',
-  columnWidth: 310
-});
+    itemSelector: '.truck-box',
+    columnWidth: 310
+  });
+
+  // SETUP SEARCH BEHAVIOUR
+
+  $('#searchbar').on('submit', function(e) {
+    e.preventDefault();
+  });
+
+  $('#searchbar input').keyup(function(){
+   // console.clear();
+   // $grid.masonry('reloadItems');
+   var valThis = $(this).val();
+   var thangs = new Array();
+     $("#all-trucks .truck-box").each(function(){
+       var text = $(this).text().toLowerCase().trim();
+       if (text.indexOf(valThis) >= 0) {
+         console.log("Found a match.");
+         // console.log(text);
+         //$(this).show()
+         //thangs.push($(this)[ 0 ]);
+         $grid.masonry('addItems', $(this)[ 0 ]).masonry('layout');
+       } else {
+         //thangs.push($(this)[ 0 ]);
+         $grid.masonry('remove', $(this)[ 0 ]).masonry('layout');
+       }
+    });
+
+    //console.log(thangs);
+    
+  });
+
+    
 });
