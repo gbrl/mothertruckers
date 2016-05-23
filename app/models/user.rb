ee21a4e4d8359af1c372e1ba5becb90ad5553c01
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-23 16:50:22 -0400

    Remove addresses for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { minimum: 1 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 1 }
   has_many :notes, dependent: :destroy
-  has_many :addresses, dependent: :destroy
   has_many :favourites, dependent: :destroy
   has_many :trucks, through: :favourites
   has_many :ratings, dependent: :destroy
