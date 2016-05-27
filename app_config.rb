11e9414e08f0240499a3a8fc2b6d477283172c49
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-27 12:40:12 -0400

    Add new DB config

--- app_config.rb
+++ app_config.rb
@@ -9,9 +9,9 @@ module AppConfig
     puts "Connecting to database..."
       ActiveRecord::Base.establish_connection(
       adapter:  "postgresql",
-      host:     "ec2-54-163-230-90.compute-1.amazonaws.com",
-      username: "ucmocwstzaxduv",
-      password: "wfx8En3oykmS2__gOkW7j_zLBG",
-      database: "d74a2i8lmqp2et")
+      host:     "ec2-23-23-162-78.compute-1.amazonaws.com",
+      username: "psdhvloxuowczo",
+      password: "dIs_fnNf-KrveKpY_tUavHxKQQ",
+      database: "d7l3t7te3413am")
   end
 end
